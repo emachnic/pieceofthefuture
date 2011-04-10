f86c419684eef199754f0faaff48807d3e01cc72
Evan Machnic <emachnic@broadmac.net>
2011-04-10 17:22:11 -0400

    New auto-responders

--- app/controllers/donate_controller.rb
+++ app/controllers/donate_controller.rb
@@ -5,7 +5,7 @@ class DonateController < ApplicationController
     if @contact_form.valid? && !@contact_form.spam?
       @contact_form.deliver
       DonationMailer.donation_email(@contact_form).deliver
-      flash[:notice] = "Email sent successfully, We will notify you if further action must be taken"
+      flash[:notice] = "Email sent successfully, we will notify you if further action must be taken. Please print this page for your records."
       redirect_to :controller => 'donations', :action => 'receipt', :id => params[:contact_form][:donation_id]
     else
       render :step_two
