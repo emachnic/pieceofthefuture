3b4551cfb38d7a0ec10f931460b95b855706d238
Evan Machnic <emachnic@broadmac.net>
2011-04-14 09:42:05 -0400

    Added asset caching

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,3 +6,13 @@ Pieceofthefuture::Application.config.session_store :cookie_store, :key => '_piec
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # Pieceofthefuture::Application.config.session_store :active_record_store
+#ActionController::Base.session = {
+#:namespace   => 'sessions',
+#:expire_after => 20.minutes.to_i,
+#:memcache_server => ['mc3.ec2.northscale.net']
+#}
+
+#require 'action_dispatch/middleware/session/dalli_store'
+#Rails.application.config.session_store :dalli_store, :memcache_server => ['mc3.ec2.northscale.net'], :namespace => 'sessions', :expire_after => 30.minutes
+
+
